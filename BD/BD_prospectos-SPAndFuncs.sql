c8d93f9adeaf2394cb24f665fe2463f3705eecc9
Alexia Valenzuela Millán <guadalual@hotmail.com>
2023-12-09 21:46:42 -0700

    SP para manipular la info de la BD

--- BD/BD_prospectos-SPAndFuncs.sql
+++ BD/BD_prospectos-SPAndFuncs.sql
@@ -6,7 +6,35 @@
 --prospectos registrados hasta el momento
 CREATE PROC sp_mostrarProspectos
 AS
-SELECT * FROM prospectos
+	SELECT * FROM prospectos
+GO
 
---comentario
+--SP_INSERTARNUEVOPROSPECTO: Agrega a la tabla PROSPECTOS un nuevo 
+--registro
+CREATE PROC sp_insertarNuevoProspecto 
+@ID INT,
+@nombre VARCHAR(50),
+@ape_paterno VARCHAR(50), 
+@ape_materno VARCHAR(50),
+@calle VARCHAR(50),
+@numero_domicilio INT,
+@colonia VARCHAR(50), 
+@codigo_postal VARCHAR(50),
+@telefono VARCHAR(10),
+@rfc VARCHAR(13),
+@estatus VARCHAR(20)
+AS
+	INSERT INTO prospectos(nombre, ape_paterno, ape_materno, calle, numero_domicilio, colonia, codigo_postal, telefono, rfc, estatus, observaciones) 
+	VALUES (@nombre ,@ape_paterno, @ape_materno, @calle, @numero_domicilio ,@colonia, @codigo_postal, @telefono,@rfc,@estatus, 'Sin observaciones relevantes')
+GO
 
+--SP_ACTUALIZAESTATUS: Actualiza el estatus y las observaciones del 
+--prospecto en caso de que tenga un estatus RECHAZADO
+CREATE PROC sp_actualizaEstatus
+@ID INT, 
+@Estatus VARCHAR(20),
+@Observaciones text
+AS
+	UPDATE prospectos SET estatus = @Estatus, observaciones = @Observaciones 
+WHERE ID = @ID
+GO
